8c330fb8773e5cf4c5013211703bb750cd786140
Tinggg <ty2326@nyu.edu>
2026-01-26 00:52:13 +0800

    Added Multitouch

--- abc-multi-touch/sketch.js
+++ abc-multi-touch/sketch.js
@@ -1,13 +1,12 @@
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5-canvas-container");
-    background(90, 200, 190);
+  background(90, 200, 190);
 }
 
 function draw() {
   fill(0);
-  circle(width/2, height/2, 100);
-
+  circle(width / 2, height / 2, 100);
 }
 
 // P5 touch events: https://p5js.org/reference/#Touch
@@ -19,20 +18,17 @@ function touchStarted() {
   // let y = touches[0].y;
   // circle(x, y, 100);
 
-  for(let i = 0; i< touches.length; i++){
-      let x = touches[i].x;
-      let y = touches[i].y;
-      circle(x, y, 100);
+  for (let i = 0; i < touches.length; i++) {
+    let x = touches[i].x;
+    let y = touches[i].y;
+    circle(x, y, 100);
   }
 }
 
-function touchMoved() {
-}
+function touchMoved() {}
 
-function touchEnded() {
-}
+function touchEnded() {}
 
-function windowResized(){
+function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
 }
-
