d2d070f247f8c10892ef21e569884d51bcf71ab6
Tinggg <ty2326@nyu.edu>
2026-01-26 01:31:48 +0800

    Update sketch.js

--- Glassdrawing/sketch.js
+++ Glassdrawing/sketch.js
@@ -1,31 +1,34 @@
+let capture;
+
 function setup() {
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5-canvas-container");
-  background(90, 200, 190);
+  capture = createCapture(VIDEO);
+  capture.hide();
+  fog = createGraphics(width, height);
+  fog.background(255, 180);
 }
 
 function draw() {
-  fill(0);
-  circle(width / 2, height / 2, 100);
+  image(capture, 0, 0, width, (width * capture.height) / capture.width);
 }
 
 // P5 touch events: https://p5js.org/reference/#Touch
 
 function touchStarted() {
   console.log(touches);
+}
 
-  // let x = touches[0].x;
-  // let y = touches[0].y;
-  // circle(x, y, 100);
-
+function touchMoved() {
   for (let i = 0; i < touches.length; i++) {
     let x = touches[i].x;
     let y = touches[i].y;
-    circle(x, y, 100);
+    fog.erase();
+    fog.circle(x, y, 80);
+    fog.noErase();
+    image(fog, 0, 0);
   }
 }
-
-function touchMoved() {}
 
 function touchEnded() {}
 
