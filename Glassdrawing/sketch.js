0c471dd7fed2b29cd5f8078076b96416db67df57
Tinggg <ty2326@nyu.edu>
2026-01-26 01:18:32 +0800

    Added Glassdrawing

---
+++ Glassdrawing/sketch.js
@@ -0,0 +1,34 @@
+function setup() {
+  let canvas = createCanvas(windowWidth, windowHeight);
+  canvas.parent("p5-canvas-container");
+  background(90, 200, 190);
+}
+
+function draw() {
+  fill(0);
+  circle(width / 2, height / 2, 100);
+}
+
+// P5 touch events: https://p5js.org/reference/#Touch
+
+function touchStarted() {
+  console.log(touches);
+
+  // let x = touches[0].x;
+  // let y = touches[0].y;
+  // circle(x, y, 100);
+
+  for (let i = 0; i < touches.length; i++) {
+    let x = touches[i].x;
+    let y = touches[i].y;
+    circle(x, y, 100);
+  }
+}
+
+function touchMoved() {}
+
+function touchEnded() {}
+
+function windowResized() {
+  resizeCanvas(windowWidth, windowHeight);
+}
