a23a3367266714fdecfbb4a5df0634b96ba4dd93
Tinggg <ty2326@nyu.edu>
2026-01-28 14:18:07 +0800

    Updated deviceOrientation

---
+++ deviceOrientation-p5/server.js
@@ -0,0 +1,41 @@
+const express = require('express');
+// const http = require("http"); // we try to make HTTPS work 
+
+const https = require("https");
+// to read certificates from the filesystem (fs)
+const fs = require("fs");
+
+const app = express(); // the server "app", the server behaviour
+
+const portHTTPS = 3000; // port for https
+// const portHTTP = 3001; // port for http
+
+
+// returning to the client anything that is
+// inside the public folder
+app.use(express.static('public'));
+
+
+// Creating object of key and certificate
+// for SSL
+const options = {
+    key: fs.readFileSync("localhost-key.pem"),
+    cert: fs.readFileSync("localhost.pem"),
+};
+
+
+
+// Creating servers and make them listen at their ports:
+https.createServer(options, app).listen(portHTTPS, function (req, res) {
+    console.log("HTTPS Server started at port", portHTTPS);
+});
+
+// if we ALSO serve on http we can incommend this, but right now we don't 
+// http.createServer(app).listen(portHTTP, function (req, res) {
+//     console.log("HTTP Server started at port", portHTTP);
+// });
+
+
+
+
+
--- deviceOrientation/public/script.js
+++ deviceOrientation/public/script.js
@@ -1,24 +1,17 @@
+// window.addEventListener("deviceorientation", handleOrientation, true); // can be deleted later
 
+function handleOrientation(eventData) {
+  console.log(eventData.alpha);
 
-window.addEventListener("deviceorientation", handleOrientation, true); // can be deleted later
+  // function handleOrientation(aaa) {
+  //   console.log(aaa);
 
+      document.querySelector('#alpha').innerText = "alpha: " + Math.round(eventData.alpha);
+      document.querySelector('#beta').innerText = "beta: " + Math.round(eventData.beta);
+      document.querySelector('#gamma').innerText = "gamma: " + Math.round(eventData.gamma);
 
-function handleOrientation(eventData){
-     console.log(e.alpha);
+      document.querySelector('h1').style.display = "none";
+      document.querySelector('#requestOrientationButton').style.display = "none";
 
-//     // document.querySelector('#alpha').innerText = "alpha: " + Math.round(eventData.alpha);
-//     // document.querySelector('#beta').innerText = "beta: " + Math.round(eventData.beta);
-//     // document.querySelector('#gamma').innerText = "gamma: " + Math.round(eventData.gamma);
-
-//     // document.querySelector('h1').style.display = "none";
-//     // document.querySelector('#requestOrientationButton').style.display = "none";
-
-//     // document.querySelector('#square').style.transform = "rotate("+eventData.alpha+"deg)";
+document.querySelector('#square').style.transform = "rotate("+eventData.alpha+"deg)";
 }
-
-
-
-
-
-
-
