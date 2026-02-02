4505f9f5df7608a7fff0fcce03336356ab957f27
Tinggg <ty2326@nyu.edu>
2026-02-02 12:34:20 +0800

    Added Simple Chat

---
+++ simple-chat/public/script.js
@@ -0,0 +1,17 @@
+let formeElm = document.querySelector("#chatForm");
+console.log(formeElm);
+let msgInput = document.querySelector("#newMessage");
+console.log(msgInput)
+
+// LISTEN FOR NEWLY TYPES MESSAGES, 
+// SEND THEM TO THE SERVER
+
+
+// LISTEN FOR NEW MESSAGES FROM SERVER
+// APPEND THEM TO THE MESSAGE BOX
+// AUTO SCROLL TO BOTTOM
+
+// APPEND MESSAGES TO BOX
+
+// OPTIONAL: LISTEN FOR NEW NAME
+// SEND IT TO SERVER
