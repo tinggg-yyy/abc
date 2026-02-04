471c7e7105e4b95aa0258e75fb756059c3c4bc8f
Tinggg <ty2326@nyu.edu>
2026-02-04 13:34:50 +0800

    Updated Chatroom

--- simple-chat/public/script.js
+++ simple-chat/public/script.js
@@ -1,17 +1,55 @@
+// initiate socket connection
+const socket = io();
+
 let formeElm = document.querySelector("#chatForm");
 console.log(formeElm);
 let msgInput = document.querySelector("#newMessage");
-console.log(msgInput)
+console.log(msgInput);
 
-// LISTEN FOR NEWLY TYPES MESSAGES, 
-// SEND THEM TO THE SERVER
+// LISTEN FOR NEWLY TYPES MESSAGES,
+formeElm.addEventListener("submit", newMessageSubmitted);
 
+function newMessageSubmitted(event) {
+  console.log("Typed a message!", event);
+  // prevent form from refreshing page
+  event.preventDefault();
+
+  let newMsg = msgInput.value;
+  console.log(newMsg);
+  //appendMessage(newMsg);
+
+  // SEND THEM TO THE SERVER
+  socket.emit("MessageFromClient", newMsg);
+
+  // clear input box
+  msgInput.value = "";
+}
 
 // LISTEN FOR NEW MESSAGES FROM SERVER
 // APPEND THEM TO THE MESSAGE BOX
 // AUTO SCROLL TO BOTTOM
+socket.on("MessageFromServer", function (msgData) {
+  console.log("got a message", msgData);
+  appendMessage(msgData.message);
+});
 
 // APPEND MESSAGES TO BOX
+function appendMessage(txt) {
+  console.log(txt);
+  // select list (ul) first
+  let chatThreadList = document.querySelector("#threadWrapper ul");
+  console.log(chatThreadList);
+
+  // create new list item (li)
+  let newListItem = document.createElement("li");
+  newListItem.innerText = txt;
+
+  // append new li to the list
+  chatThreadList.append(newListItem);
+
+  // scroll to bottom of textbox:
+  chatThreadList.scrollTop = chatThreadList.scrollHeight;
+}
 
 // OPTIONAL: LISTEN FOR NEW NAME
 // SEND IT TO SERVER
--- simple-chat/server.js
+++ simple-chat/server.js
@@ -1,5 +1,5 @@
-const express = require('express');
-// const http = require("http"); // we try to make HTTPS work 
+const express = require("express");
+// const http = require("http"); // we try to make HTTPS work
 
 const https = require("https");
 // to read certificates from the filesystem (fs)
@@ -10,39 +10,50 @@ const app = express(); // the server "app", the server behaviour
 const portHTTPS = 3000; // port for https
 // const portHTTP = 3001; // port for http
 
-
 // returning to the client anything that is
 // inside the public folder
-app.use(express.static('public'));
-
+app.use(express.static("public"));
 
 // Creating object of key and certificate
 // for SSL
 const options = {
-    key: fs.readFileSync("keys-for-local-https/localhost-key.pem"),
-    cert: fs.readFileSync("keys-for-local-https/localhost.pem"),
+  key: fs.readFileSync("keys-for-local-https/localhost-key.pem"),
+  cert: fs.readFileSync("keys-for-local-https/localhost.pem"),
 };
 
 const HTTPSserver = https.createServer(options, app);
-const {Server} = require("socket.io"); // include library
+const { Server } = require("socket.io"); // include library
 const io = new Server(HTTPSserver); // start socket.io server
 
 // handling socket.io connections
-io.on('connection', (socket) => {
-    console.log('a user connected:');
+io.on("connection", (socket) => {
+  console.log("a user connected:");
+
+  socket.on("MessageFromClient", function (incomingMessage) {
+    console.log(incomingMessage);
+
+    // SEND MESSAGE TO ALL OTHER CONNECTED CLIENTS
+    let messageToAllClients = {
+        sender:"unknown",
+        message: incomingMessage
+    }
+
+    // Server emits to all connected clients
+     io.emit("MessageFromServer", messageToAllClients);
+
+  });
+
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 });
 
 // Creating servers and make them listen at their ports:
 HTTPSserver.listen(portHTTPS, function (req, res) {
-    console.log("HTTPS Server started at port", portHTTPS);
+  console.log("HTTPS Server started at port", portHTTPS);
 });
 
-// if we ALSO serve on http we can incommend this, but right now we don't 
+// if we ALSO serve on http we can incommend this, but right now we don't
 // http.createServer(app).listen(portHTTP, function (req, res) {
 //     console.log("HTTP Server started at port", portHTTP);
 // });
-
-
-
-
-
