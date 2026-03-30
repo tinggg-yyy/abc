cf7958edd3348b37cad6f151dd07cbd038c81224
Tinggg <ty2326@nyu.edu>
2026-03-30 11:32:41 +0800

    Update server.js

--- TonyX/server.js
+++ TonyX/server.js
@@ -1,3 +1,14 @@
+// let socket = io();
+// socket connection that works locally and on the server:
+if (
+  location.hostname.toLowerCase().startsWith("browsercircus") ||
+  location.hostname.toLowerCase().startsWith("www")
+) {
+  socket = io({ path: "/ting/port-4280/socket.io" }); // e.g. '/leon/port-4100/socket.io' or '/socket.io'
+} else {
+  socket = io();
+}
+
 // GPS HAIR PROJECT — server.js
 
 const express = require("express");
