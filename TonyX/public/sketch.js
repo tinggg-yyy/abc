10dbaa3262f675627ab87f804bd6a7c341d1a7c2
Tinggg <ty2326@nyu.edu>
2026-04-01 01:13:24 +0800

    cc

--- TonyX/public/sketch.js
+++ TonyX/public/sketch.js
@@ -29,7 +29,7 @@ if (
   location.hostname.toLowerCase().startsWith("browsercircus") ||
   location.hostname.toLowerCase().startsWith("www")
 ) {
-  socket = io({ path: "/luna/port-4240/socket.io" });
+  socket = io({ path: "/luna/port-4280/socket.io" });
 } else {
   socket = io();
 }
--- TonyX/server.js
+++ TonyX/server.js
@@ -13,7 +13,7 @@ const HTTPSserver = https.createServer(options, app);
 const { Server } = require("socket.io");
 const io = new Server(HTTPSserver);
 
-const PORT = 4240;
+const PORT = 4280;
 
 const headCx = 0.2;
 const headCy = -0.38;
