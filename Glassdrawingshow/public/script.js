f449bef65baa670ece5aafe13de1348f190d4da6
Tinggg <ty2326@nyu.edu>
2026-05-08 15:25:28 +0800

    Updated port

--- Glassdrawingshow/public/script.js
+++ Glassdrawingshow/public/script.js
@@ -4,7 +4,7 @@ if (
   location.hostname.toLowerCase().startsWith("browsercircus") ||
   location.hostname.toLowerCase().startsWith("www")
 ) {
-  socket = io({ path: "/ting/port-4280/socket.io" }); // e.g. '/leon/port-4100/socket.io' or '/socket.io'
+  socket = io({ path: "/ting/port-4281/socket.io" }); // e.g. '/leon/port-4100/socket.io' or '/socket.io'
 } else {
   socket = io();
 }
